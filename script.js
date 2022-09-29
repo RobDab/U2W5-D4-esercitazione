fed9223d315eb3a0a46c3f5c5ca95c759327f8a7
RobDab <roberto.dabb@gmail.com>
2022-09-29 22:49:38 +0200

    implemented functions

--- script.js
+++ script.js
@@ -23,7 +23,11 @@ function dropMenu(){
 }
 
 let text = document.getElementById('text')
+
 function setStylePoppins(){
+    if(text.className.includes('robotoText')){
+        text.classList.remove('robotoText')
+    }
     text.classList.toggle('poppinsText')
 }
 
@@ -35,3 +39,58 @@ function setStyleRoboto(){
 function setStyleBold(){
     text.classList.toggle('boldText')
 }
+
+let inputField = document.getElementById('toDoList').querySelector('input')
+
+
+
+
+function addListItem(){
+    
+    let inputValue = inputField.value
+    
+    if(inputValue !== ""){
+        console.log(inputValue)
+    let list = document.getElementById('list')
+    list.innerHTML += `<li>${inputValue}<button>-</button></li>`
+
+    inputField.value = ""   //CLEAR INPUT FIELD
+    }
+    
+    let listItemS = document.getElementById('list').querySelectorAll('li')
+    for(let i=0;i<listItemS.length;i++){
+        listItemS[i].querySelector('button').addEventListener("click",remove)
+        // listItemS[i].querySelector('button').id = `${i}`
+        // console.log(listItemS[i].querySelector('button').id)
+        }
+    
+}
+
+// let i
+// let buttons = document.getElementById('list').querySelectorAll('li')
+//     //^^^NODES ARRAY^^^
+//     for(let i=0;i<buttons.length;i++){
+//     buttons[i].querySelector('button').addEventListener("click",remove)
+//     }
+    //AGGIUNGE ONLCICK A TUTTI 
+
+function remove(){
+    
+    let listItemS = document.getElementById('list').querySelectorAll('li')
+    
+    // CHIEDERE A LIDIA!!!!!
+    console.log(listItemS)
+
+      
+}
+// COME FACCIO A SELEZIONARE SOLO IL <LI> IN CUI IL BUTTON è CONTENUTO???
+
+
+
+
+
+
+
+
+
+
